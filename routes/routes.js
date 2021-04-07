35206e8d34c780599c86bc0f0d6c7de960e00b22
returnKitten <12476396+returnKitten@users.noreply.github.com>
2021-04-07 18:18:41 +0100

    fix

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
 	if (!req.session.cognitoData) {
-        res.render('login');
+        res.render('login', {});
     } else {
         res.render('index', {
             static_path: 'static',
