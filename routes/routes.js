aadcaca9e8f6a04a46fc400131b9c9f4cd91653e
returnKitten <12476396+returnKitten@users.noreply.github.com>
2021-04-07 18:14:06 +0100

    adding routes folder

--- routes/routes.js
+++ routes/routes.js
@@ -1,1 +1,14 @@
-a
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+	if (!req.session.cognitoData) {
+        res.render('login');
+    } else {
+        res.render('index', {
+            static_path: 'static',
+        });
+    }
+})
+
+module.exports = router
