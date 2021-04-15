99bdf53634dcc2c666bd9c1dde45415819aa05da
returnKitten <12476396+returnKitten@users.noreply.github.com>
2021-04-15 14:35:52 +0100

    fix

--- services/speech.js
+++ services/speech.js
@@ -60,9 +60,6 @@ module.exports = function (credentials, config) {
 
     SpeechService.prototype.createAdaptationsFromConfig = async function () {
 
-        // todo remove
-        await this.deletePhraseSet('order');
-
         console.log('\nCreating Custom Classes and Phrase sets from adaptation config...')
         await this.createCustomClassesFromArray(config.stt.adaptations.configuration.customClasses);
         await this.createPhraseSetsFromArray(config.stt.adaptations.configuration.phraseSets);
