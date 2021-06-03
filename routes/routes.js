f7b53b3d30a25c0639a07e2bfbdf0ae4842d270c
Filipe <filipe.b.aleixo@gmail.com>
2021-06-03 13:55:28 +0100

    Removing login window from app.vorder.io

--- routes/routes.js
+++ routes/routes.js
@@ -2,25 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	if (!req.session.cognitoData) {
-        res.render('login');
-
-        // EXPERIMENTING FOR COGNITO TOKENS
-        const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
-        var poolData = {
-            UserPoolId: 'us-east-1_wKO7h3kGU', // Your user pool id here
-            ClientId: '32tdipotlt2o43i8m7oq3mjncq', // Your client id here
-        };
-        var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-        var cognitoUser = userPool.getCurrentUser();
-        //console.log(cognitoUser);
-        // END EXPERIMENT
-
-    } else {
-        res.render('index', {
-            static_path: 'static',
-        });
-    }
+    res.render('index', {
+        static_path: 'static',
+    });
 })
 
 module.exports = router
