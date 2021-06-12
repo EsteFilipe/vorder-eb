77f33c1cfd256d183658591ab4b95e11b3323bef
Filipe <filipe.b.aleixo@gmail.com>
2021-06-12 17:30:33 +0100

    elevated chmod permission to write

--- .platform/hooks/postdeploy/create-jwt-public-keys-folder.sh
+++ .platform/hooks/postdeploy/create-jwt-public-keys-folder.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p /var/app/current/public_keys/
 touch /var/app/current/public_keys/jwks.json
-chmod 755 /var/app/current/public_keys/jwks.json
+chmod 777 /var/app/current/public_keys/jwks.json
