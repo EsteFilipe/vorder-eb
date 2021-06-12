f7bdfbb7096b98fe6a66374711e0cc29c4050b47
Filipe <filipe.b.aleixo@gmail.com>
2021-06-12 16:47:24 +0100

    didn't work. changing hook to post and also changing target dir

---
+++ .platform/hooks/post/create-jwt-public-keys-folder.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir -p /var/app/current/public_keys/
+chmod 755 /var/app/current/public_keys/
--- .platform/hooks/prebuild/create-jwt-public-keys-folder.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir -p /home/public_keys/
-chmod 755 /home/public_keys/public_keys/
