3eacf6e12d56fd786d667e3af9be4aa43b4b02a7
Filipe <filipe.b.aleixo@gmail.com>
2021-06-12 17:16:07 +0100

    Alright, public_keys folder now gets created, but nodejs can't write the file there. Updated .sh file

--- .platform/hooks/postdeploy/create-jwt-public-keys-folder.sh
+++ .platform/hooks/postdeploy/create-jwt-public-keys-folder.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mkdir -p /var/app/current/public_keys/
-chmod 755 /var/app/current/public_keys/
+touch /var/app/current/public_keys/jwks.json
+chmod 755 /var/app/current/public_keys/jwks.json
